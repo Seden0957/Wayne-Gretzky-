cc33e7f5b68c41e4913576aa7437f55ab036fd98
seden0957 <seden0957@7KXL6Y1.Student.UGDSB.ED>
2018-03-07 13:57:06 -0500

    6head

---
+++ src/wayne/gretzky/WayneGretzky.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wayne.gretzky;
+
+/**
+ *
+ * @author seden0957
+ */
+public class WayneGretzky {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Wayne Gretzky";
+        int playerNumber = 99;
+        System.out.println(name + " is number " + playerNumber);
+    }
+    
+}
+
+    
+    
+
